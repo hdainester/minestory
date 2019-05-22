cfa290e2333471353faffaf6bb003e97fc7e3709
hdainester <27729552+hdainester@users.noreply.github.com>
2019-05-23 01:18:10 +0200

    - Program: added offline command line option

--- src/Program.cs
+++ src/Program.cs
@@ -10,7 +10,7 @@ namespace Chaotx.Minestory {
                 DropboxConnect.AccessToken = File.ReadAllText("dbxacctok");
             } catch(Exception e) {
                 Console.WriteLine("could not load dbxacctok: " + e.Message);
-            } else if(args.Length == 1)
+            } else if(args.Length == 1 && !args[0].Equals("offline"))
                 DropboxConnect.AccessToken = args[0];
 
             Task task = DropboxConnect.Connect();
